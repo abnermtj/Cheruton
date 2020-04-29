9ad93b9383864d91942a8ac32ad45260a0ebdb99
theopin <52351742+theopin@users.noreply.github.com>
2020-04-29 13:15:26 +0800

    Add files via upload

---
+++ ExpBar/ExpBar.gd
@@ -0,0 +1,32 @@
+extends Control
+
+var old_exp
+var old_level
+var old_exp_max 
+func _ready():	
+	DataFunctions.connect("update_exp", self, "update_expbar")
+	DataResource.connect("loaded", self, "initbar")
+
+
+func initbar():
+	old_exp = DataResource.dict_player["exp_curr"]
+	old_exp_max = DataResource.dict_player["exp_max"]
+	old_level = DataResource.dict_player["level"]
+	if(old_exp_max != 0):
+		$ExpBar.value = old_exp/old_exp_max * 100
+
+func update_expbar(new_exp, new_exp_max, new_level):
+	
+	while(old_level < new_level):
+		animate_expbar($ExpBar.value, 100)
+		yield(get_tree().create_timer(0.2), "timeout")
+		$ExpBar.value = 0
+		old_level+=1
+		
+	animate_expbar($ExpBar.value, new_exp/new_exp_max * 100)
+	old_exp = new_exp
+
+func animate_expbar(start, end):
+	$Tween.interpolate_property($ExpBar, "value", start, end, 0.2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
+	
