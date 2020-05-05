8b5d7845032aeacd1b4652bc7584b5e5eb5c34cf
theopin <theopinto98@gmail.com>
2020-05-05 22:28:54 +0800

    partial works (pause)

--- Cheruton v1.0/AutoLoad/KeyPress.gd
+++ Cheruton v1.0/AutoLoad/KeyPress.gd
@@ -1,9 +1,20 @@
 extends Node
 
+var current_scene
+
+
+
 const PAUSE = preload("res://Display/Pause/Pause.tscn")
 #need to disable keyboard input at welcome, mmenu settings
-func _input(ev): # need to fix first
+#fix the freeup and button press - buggy
+func _input(ev): 
+	Pause.connect("free_pause", self, "close_popup")
 	if Input.is_key_pressed(KEY_ESCAPE):
 		# Instance the new scene.
-		var current_scene = PAUSE.instance()
-		PAUSE.popup()
+		DataResource.current_scene.hide()
+		current_scene = PAUSE.instance()
+		self.add_child(current_scene)
+		current_scene.popup()
+
+func close_popup():
+	current_scene.queue_free()
--- Cheruton v1.0/Display/Pause/Pause.gd
+++ Cheruton v1.0/Display/Pause/Pause.gd
@@ -1,5 +1,5 @@
 extends Popup
-
+signal free_pause
 const SETTINGS = "res://Display/Settings/Settings.tscn"
 const MAINMENU = "res://Display/MainMenu/MainMenu.tscn"
 
@@ -7,12 +7,15 @@ func _ready():
 	pass
 
 func _on_ExitDirect_pressed():
-	get_tree().exit()
+	#save player_data?
+	get_tree().quit()
 
 
 func _on_Settings_pressed():
+	emit_signal("free_pause")
 	LoadGlobal.goto_scene(SETTINGS)
-
+	
 
 func _on_RMMenu_pressed():
 	LoadGlobal.goto_scene(MAINMENU)
+
