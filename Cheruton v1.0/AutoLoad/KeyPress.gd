32fe6bd814cf7e43a52141108af183581f888639
theopin <theopinto98@gmail.com>
2020-05-06 21:14:29 +0800

    pause->settings->pause fix

--- Cheruton v1.0/AutoLoad/KeyPress.gd
+++ Cheruton v1.0/AutoLoad/KeyPress.gd
@@ -7,15 +7,19 @@ const INVENTORY = preload("res://Player/Inventory/Inventory.tscn")
 func _input(ev): 
 	if Input.is_key_pressed(KEY_ESCAPE):
 		#Prevents instancing during non-gameplay scenes
-		if (DataResource.dict_settings["game_on"] == false):
-			return
-		var curr_scene = PAUSE.instance()
-		DataResource.current_scene.add_child(curr_scene)
-		curr_scene.popup()
+		if (DataResource.dict_settings["game_on"] == true):
+			escape_key()
 
 	elif Input.is_key_pressed(KEY_I):
-		if (DataResource.dict_settings["game_on"] == false):
-			return
-		var curr_scene = INVENTORY.instance()
-		DataResource.current_scene.add_child(curr_scene)
+		if (DataResource.dict_settings["game_on"] == true):
+			i_key()
 
+
+func escape_key():
+	var curr_scene = PAUSE.instance()
+	DataResource.current_scene.add_child(curr_scene)
+	curr_scene.popup()
+
+func i_key():
+	var curr_scene = INVENTORY.instance()
+	DataResource.current_scene.add_child(curr_scene)	
