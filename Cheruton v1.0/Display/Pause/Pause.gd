e7aeb1bc6745839a45aa857827a2e9bb0eb32e4b
theopin <theopinto98@gmail.com>
2020-05-09 10:12:07 +0800

    Revert "Revert "can use the same key to remove instance of iven/pause""
    This reverts commit 2c0b01f5767ad42d6b4c28e4408bfa030f973140.

--- Cheruton v1.0/Display/Pause/Pause.gd
+++ Cheruton v1.0/Display/Pause/Pause.gd
@@ -7,26 +7,28 @@ const MAINMENU = "res://Display/MainMenu/MainMenu.tscn"
 func _ready():
 	DataResource.dict_settings["game_on"] = false
 
+#exiting to mm/exit needs to warn player about unsaved data
+
+
 func _on_ExitDirect_pressed():
-	#save player_data?
+
 	get_tree().quit()
 
 
 func _on_Settings_pressed():
+	DataResource.dict_settings["maj_scn"] = true
 	DataResource.current_scene.hide()
 	var curr_scene = SETTINGS.instance()
 	get_tree().get_root().add_child(curr_scene)
 
 
 func _on_RMMenu_pressed():
+	DataResource.dict_settings["maj_scn"] = true
 	LoadGlobal.goto_scene(MAINMENU)
 
 func _on_ExitPause_pressed():
 	free_the_pause()
 
-
-func _on_Pause_popup_hide():
-	free_the_pause()
 
 func free_the_pause():
 	var scene_to_free = DataResource.current_scene.get_child(DataResource.current_scene.get_child_count() - 1)
