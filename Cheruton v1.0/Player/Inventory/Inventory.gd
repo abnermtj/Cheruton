32c2694059a131981727db4adbcd9551141d0b30
theopin <theopinto98@gmail.com>
2020-05-11 10:07:23 +0800

    added feature to highlight selected tab of inventory

--- Cheruton v1.0/Player/Inventory/Inventory.gd
+++ Cheruton v1.0/Player/Inventory/Inventory.gd
@@ -1,15 +1,21 @@
 extends Control
 
+var active_tab
 var count = 0
 
+onready var active_tab_image = preload("res://Player/Inventory/inventory_bg_keypress.png")
+onready var default_tab_image = preload("res://Player/Inventory/inventory_bg.png")
 
 func _ready():
 	DataResource.dict_settings["game_on"] = false
 	$BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Coins/CoinsVal.text = str(DataResource.dict_player["coins"])
+
 	# Hide initbar() to view inventory directly
-	
 	#$BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/ExpBar.initbar()
 	#$BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/HealthBar.initbar()
+	#change_active_tab($BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Weapons/Weapons)
+
+
 
 func _on_Exit_pressed():
 	free_the_inventory()
@@ -19,7 +25,13 @@ func free_the_inventory():
 	var scene_to_free = DataResource.current_scene.get_child(DataResource.current_scene.get_child_count() - 1)
 	scene_to_free.queue_free()
 
-
+func change_active_tab(new_tab):
+	# Set current tab to default colour
+	if(active_tab):
+		active_tab.set_normal_texture(default_tab_image)
+	# Set new active tab and its colour
+	active_tab = new_tab
+	active_tab.set_normal_texture(active_tab_image)
 
 #func _on_Test_pressed(): # creates a double click signal and activates tooltips
 #	if(count == 0):
@@ -33,3 +45,23 @@ func free_the_inventory():
 #
 #func _on_CountDown_timeout():
 #	count = 0
+
+
+func _on_Weapons_pressed():
+	change_active_tab($BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Weapons/Weapons)
+
+
+func _on_Apparel_pressed():
+	change_active_tab($BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Apparel/Apparel)
+
+
+func _on_Aid_pressed():
+	change_active_tab($BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Aid/Aid)
+
+
+func _on_Misc_pressed():
+	change_active_tab($BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Misc/Misc)
+
+
+func _on_KeyItem_pressed():
+	change_active_tab($BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/KeyItem/KeyItem)
