4ae826465bc0de6b5b4225dd0994efa57c5baf87
theopin <theopinto98@gmail.com>
2020-05-11 10:27:08 +0800

    data for respective tab is shown when clicked

--- Cheruton v1.0/Player/Inventory/Inventory.gd
+++ Cheruton v1.0/Player/Inventory/Inventory.gd
@@ -2,6 +2,7 @@ extends Control
 
 var active_tab
 var count = 0
+var active_tab_items
 
 onready var active_tab_image = preload("res://Player/Inventory/inventory_bg_keypress.png")
 onready var default_tab_image = preload("res://Player/Inventory/inventory_bg.png")
@@ -9,12 +10,13 @@ onready var default_tab_image = preload("res://Player/Inventory/inventory_bg.png
 func _ready():
 	DataResource.dict_settings["game_on"] = false
 	$BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Coins/CoinsVal.text = str(DataResource.dict_player["coins"])
-
+	# Weapons-Default
+	_on_Weapons_pressed()
+	
 	# Hide initbar() to view inventory directly
 	#$BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/ExpBar.initbar()
 	#$BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/HealthBar.initbar()
-	#change_active_tab($BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Weapons/Weapons)
-
+	
 
 
 func _on_Exit_pressed():
@@ -25,13 +27,43 @@ func free_the_inventory():
 	var scene_to_free = DataResource.current_scene.get_child(DataResource.current_scene.get_child_count() - 1)
 	scene_to_free.queue_free()
 
-func change_active_tab(new_tab):
+func change_active_tab(new_tab, items_list):
 	# Set current tab to default colour
 	if(active_tab):
 		active_tab.set_normal_texture(default_tab_image)
+		active_tab_items.hide()
 	# Set new active tab and its colour
 	active_tab = new_tab
+	active_tab_items = items_list
 	active_tab.set_normal_texture(active_tab_image)
+	active_tab_items.show()
+
+
+
+func _on_Weapons_pressed():
+	var tab = $BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Weapons/Weapons
+	var list = $BorderBackground/InnerBackground/VBoxContainer/MElements/Weapons
+	change_active_tab(tab, list)
+
+func _on_Apparel_pressed():
+	var tab = $BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Apparel/Apparel
+	var list = $BorderBackground/InnerBackground/VBoxContainer/MElements/Apparel
+	change_active_tab(tab, list)
+
+func _on_Aid_pressed():
+	var tab = $BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Aid/Aid
+	var list = $BorderBackground/InnerBackground/VBoxContainer/MElements/Aid
+	change_active_tab(tab, list)
+
+func _on_Misc_pressed():
+	var tab = $BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Misc/Misc
+	var list = $BorderBackground/InnerBackground/VBoxContainer/MElements/Misc
+	change_active_tab(tab, list)
+
+func _on_KeyItems_pressed():
+	var tab = $BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/KeyItems/KeyItems
+	var list = $BorderBackground/InnerBackground/VBoxContainer/MElements/KeyItems
+	change_active_tab(tab, list)
 
 #func _on_Test_pressed(): # creates a double click signal and activates tooltips
 #	if(count == 0):
@@ -45,23 +77,3 @@ func change_active_tab(new_tab):
 #
 #func _on_CountDown_timeout():
 #	count = 0
-
-
-func _on_Weapons_pressed():
-	change_active_tab($BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Weapons/Weapons)
-
-
-func _on_Apparel_pressed():
-	change_active_tab($BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Apparel/Apparel)
-
-
-func _on_Aid_pressed():
-	change_active_tab($BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Aid/Aid)
-
-
-func _on_Misc_pressed():
-	change_active_tab($BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/Misc/Misc)
-
-
-func _on_KeyItem_pressed():
-	change_active_tab($BorderBackground/InnerBackground/VBoxContainer/MElements/Tabs/KeyItem/KeyItem)
