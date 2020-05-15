3a389a42528d939d72cac99dc120502d576b573e
Abner <abnermtj@live.com.sg>
2020-05-15 21:31:53 +0800

    merge

--- Cheruton v1.0/Player/ActualPlayer/Hook/Chain.gd
+++ Cheruton v1.0/Player/ActualPlayer/Hook/Chain.gd
@@ -3,21 +3,16 @@ extends Node2D
 const SPEED = 50	# The speed with which the chain moves
 
 onready var links = $links		# caching
-onready var danglinglinks = $danglinglinks
 
 var direction := Vector2(0,0)	# The direction in which the chain was shot
 var tip := Vector2(0,0)			# stores global position not local of the tip(local changes with player movement)
 
 var chain_in_air
 var hooked
-var dangling_chain
 var end_loop # This is the loop player hangs from, not a vector!
-signal dangling_setup_done
 
 func shoot(dir: Vector2) -> void:
 	links.visible = true
-	danglinglinks.visible = false
-
 	direction = dir # assume normalized
 	chain_in_air = true
 	tip = self.global_position
@@ -25,35 +20,27 @@ func shoot(dir: Vector2) -> void:
 func release() -> void:
 	chain_in_air = false
 	hooked = false
-	danglinglinks.setup_done = false
-	danglinglinks.remove_links()
 
 func _physics_process(_delta: float) -> void:
 	self.visible = chain_in_air or hooked
 	if not self.visible:
 		return
-	if not hooked: # move an draw until collision
-		var tip_loc = to_local(tip)
+#	if not hooked: # move an draw until collision
+	var tip_loc = to_local(tip)
 
-		links.rotation = self.position.angle_to_point(tip_loc) - deg2rad(90)
-		$tip.rotation = links.rotation
+	links.rotation = self.position.angle_to_point(tip_loc) - deg2rad(90)
+	$tip.rotation = links.rotation
 
-		links.position = tip_loc 	# link moved to attach to the tip
-		links.region_rect.size.y = tip_loc.length()		# This extends the link's repeated texture
+	links.position = tip_loc 	# link moved to attach to the tip
+	links.region_rect.size.y = tip_loc.length()		# This extends the link's repeated texture
 
-		# this is meant to save the global position in between the delta between two physics frames in which play could move and thus affect global position of tip which is its grandhcild
-		$tip.global_position = tip
-		if chain_in_air:
-			if $tip.move_and_collide(direction * SPEED):
-				hooked = true
-				chain_in_air = false
-				danglinglinks.start_position = to_global(Vector2())
-				danglinglinks.fixed_end_pos = tip
-		tip = $tip.global_position	# set `tip` as starting position for next frame
+	# this is meant to save the global position in between the delta between two physics frames in which play could move and thus affect global position of tip which is its grandhcild
+	$tip.global_position = tip
+	if chain_in_air:
+		if $tip.move_and_collide(direction * SPEED):
+			hooked = true
+			chain_in_air = false
+	tip = $tip.global_position	# set `tip` as starting position for next frame
 
-	else : # hooked, swinging state
-		links.visible = false
-		danglinglinks.visible = true
-
-func _on_danglinglinks_setup_done_chain():
-	emit_signal("dangling_setup_done")
+#	else : # hooked, swinging state
+#		links.visible = false
--- Cheruton v1.0/Player/ActualPlayer/States/motion/hook.gd
+++ Cheruton v1.0/Player/ActualPlayer/States/motion/hook.gd
@@ -4,7 +4,7 @@ const PLAYER_ENTER_SLOWDOWN  = 1 # delete if don't need to slow player when ente
 const PULL_DOWN_STRENGTH = 0.55
 const PULL_UP_STRENGTH = 1.65
 const CHAIN_STRENGTH_FAST = 1000
-const CHAIN_STRENGTH_SLOW = 0
+const CHAIN_STRENGTH_SLOW = 300
 const MAX_SPEED = 800
 const ZIP_TIMER = .200 # time give to player to zip up before falling again
 const RELEASE_TIMER = .100 # time give to player to zip up before falling again
@@ -15,6 +15,10 @@ var chain_pull
 var chain_velocity = Vector2(0,0)
 var zip_state
 var chain_set
+
+var x_rot # rotation position
+var y_rot
+var angular_vel = .05
 
 func enter():
 	chain_set = false
@@ -35,44 +39,34 @@ func update(delta):
 	release_timer -= delta
 	if Input.is_action_pressed("jump") and release_timer < 0: # bugs if jumping same frame as press
 		emit_signal("finished", "previous")
-	elif not Input.is_action_pressed("hook"): # window to zip up
-		zip_timer -= delta
-		if zip_timer < 0:
-			emit_signal("finished", "previous")
-		return
 
-#	if chain_set:
-#		owner.global_position = owner.get_node("chain").end_loop.global_position
-#		print(owner.get_node("chain").end_loop)
-#		print("glob ", owner.global_position)
-#		print("Hook",owner.get_node("chain").end_loop.global_position) #global position here does not update when chains moves on its own
-#		#zipup
-#		if Input.is_action_just_pressed("hook"):
-#			chain_pull = CHAIN_STRENGTH_FAST
-#			zip_state = true
-#		else: #
-#			chain_velocity = (owner.get_node("chain").get_node("tip").position).normalized() * chain_pull
-#			#important else gravity going to stack with pulldown, drop velocity drops very fast
-#			if chain_velocity.y > 0:
-#				chain_velocity.y *= PULL_DOWN_STRENGTH
-#			else:
-#				chain_velocity.y *= PULL _UP_STRENGTH
-#	else:
-#		chain_velocity = Vector2(0,0)
-#
-##	if zip_state == true:
-##		owner.velocity = chain_velocity
-##	else:
-##		owner.velocity.y += owner.GRAVITY * delta # normal gravity when player not hooked yet
-#
-#	owner.move_and_slide(owner.velocity, Vector2.UP)
+	zip_timer -= delta
+	#zipup
+	if Input.is_action_just_pressed("hook") and zip_timer < 0:
+		chain_pull = CHAIN_STRENGTH_FAST
+		zip_state = true
+	else:
+		chain_velocity = (owner.get_node("chain").get_node("tip").position).normalized() * chain_pull
 
-	owner.velocity.y = clamp(owner.velocity.y, -MAX_SPEED, MAX_SPEED)
-	owner.velocity.x = clamp(owner.velocity.x, -MAX_SPEED, MAX_SPEED)
+		#important else gravity going to stack with pulldown, drop velocity drops very fast
+		if chain_velocity.y > 0:
+			chain_velocity.y *= PULL_DOWN_STRENGTH
+		else:
+			chain_velocity.y *= PULL_UP_STRENGTH
+
+	if zip_state == true:
+		owner.velocity = chain_velocity
+	else:
+		var tip = owner.get_node("chain").tip
+		angular_vel += Vector2(owner.global_position.x - tip.x, owner.global_position.y - tip.y).normalized().x  *.01# creates swinging
+		angular_vel *= .01
+		x_rot = cos(angular_vel) * (owner.global_position.x - tip.x) - sin(angular_vel) * (owner.global_position.y - tip.y) + tip.x
+		y_rot = sin(angular_vel) * (owner.global_position.x - tip.x) + cos(angular_vel) * (owner.global_position.y - tip.y) + tip.y
+		owner.global_position = Vector2(x_rot, y_rot)
+	#owner.move_and_slide(owner.velocity, Vector2.UP)
+
+	#owner.velocity.y = clamp(owner.velocity.y, -MAX_SPEED, MAX_SPEED)
+	#owner.velocity.x = clamp(owner.velocity.x, -MAX_SPEED, MAX_SPEED)
 
 func exit():
 	owner.get_node("chain").release()
-
-
-func _on_chain_dangling_setup_done():
-	chain_set = true
