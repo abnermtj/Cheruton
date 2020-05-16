b27db1ddbad2b743fd4177a2ddb49ceb63aa833d
theopin <theopinto98@gmail.com>
2020-05-16 15:56:18 +0800

    code cleanup: append_loot()

--- Cheruton v1.0/AutoLoad/Loot.gd
+++ Cheruton v1.0/AutoLoad/Loot.gd
@@ -43,75 +43,34 @@ func append_loot(loot_count):
 		# Append current_index of loot dict to the temp ivnentory dict
 		# If inventory has the item, increase its item quantity
 		# else, create new data of its stats in dict_inventory
-
-		if(loot_dict[index][0] == "Weapons"):
-			print("Appending to Weapons")
-			if(DataResource.dict_inventory.Weapons.size() == 0):
-				print("Item Not Present. Inserting...")
-				var curr_size = DataResource.dict_inventory.Weapons.size() + 1
-				insert_data(index, curr_size)
-				pass
-			
-			for i in range(1, DataResource.dict_inventory.Weapons.size() + 1):
-				if(DataResource.dict_inventory.Weapons["Item" + str(i)].item_name == loot_dict[index][1]):
-					print("Item Present")
-					DataResource.dict_inventory.Weapons["Item" + str(i)].item_qty += loot_dict[index][2]
-					break
-				elif(i == DataResource.dict_inventory.Weapons.size()):
-					print("Item Not Present. Inserting...")
-					var curr_size = DataResource.dict_inventory.Weapons.size() + 1
-					insert_data(index, curr_size)
-
-		elif(loot_dict[index][0] == "Apparel"):
-			print("Appending to Apparel")
-			for i in range(1, DataResource.dict_inventory.Apparel.size() + 1):
-				if(DataResource.dict_inventory.Apparel["Item" + str(i)].item_name == loot_dict[index][1]):
-					print("Item Present")
-					DataResource.dict_inventory.Apparel["Item" + str(i)].item_qty += loot_dict[index][2]
-					break
-				elif(i == DataResource.dict_inventory.Apparel.size()):
-					print("Item Not Present. Inserting...")
-					var curr_size = DataResource.dict_inventory.Apparel.size() + 1
-					insert_data(index, curr_size)
-
-		elif(loot_dict[index][0] == "Consum"):
-			print("Appending to Consum")
-			for i in range(1, DataResource.dict_inventory.Consum.size() + 1):
-				if(DataResource.dict_inventory.Consum["Item" + str(i)].item_name == loot_dict[index][1]):
-					print("Item Present")
-					DataResource.dict_inventory.Consum["Item" + str(i)].item_qty += loot_dict[index][2]
-					break
-				elif(i == DataResource.dict_inventory.Consum.size()):
-					print("Item Not Present. Inserting...")
-					var curr_size = DataResource.dict_inventory.Consum.size() + 1
-					insert_data(index, curr_size)
-
-		elif(loot_dict[index][0] == "Misc"):
-			print("Appending to Misc")
-			for i in range(1, DataResource.dict_inventory.Misc.size() + 1):
-				if(DataResource.dict_inventory.Misc["Item" + str(i)].item_name == loot_dict[index][1]):
-					print("Item Present")
-					DataResource.dict_inventory.Misc["Item" + str(i)].item_qty += loot_dict[index][2]
-					break
-				elif(i == DataResource.dict_inventory.Misc.size()):
-					print("Item Not Present. Inserting...")
-					var curr_size = DataResource.dict_inventory.Misc.size() + 1
-					insert_data(index, curr_size)
-
-		elif(loot_dict[index][0] == "Key Items"):
-			print("Appending to Key Items")
-			for i in range(1, DataResource.dict_inventory["Key Items"].size() + 1):
-				if(DataResource.dict_inventory["Key Items"]["Item" + str(i)].item_name == loot_dict[index][1]):
-					print("Item Present")
-					DataResource.dict_inventory["Key Items"]["Item" + str(i)].item_qty += loot_dict[index][2]
-					break
-				elif(i == DataResource.dict_inventory["Key Items"].size()):
-					print("Item Not Present. Inserting...")
-					var curr_size = DataResource.dict_inventory["Key Items"].size() + 1
-					insert_data(index, curr_size)
-		elif(loot_dict[index][0] == "Money"):
-			print("Increasing Coins")
+		var name = loot_dict[index][0]
+		print("Appending to " + name)
+		
+		# Increase Coins
+		if(loot_dict[index][0] == "Money"):
 			DataFunctions.change_coins(int(loot_dict[index][2]))
+			
+		# Insert item in inventory
+		else:
+			# Empty Tab
+			if(DataResource.dict_inventory[name].size() == 0):
+				print("Item Not Present. Inserting...")
+				var curr_size = DataResource.dict_inventory[name].size() + 1
+				insert_data(index, curr_size)
+
+			# Non-Empty Tab
+			else:
+				for i in range(1, DataResource.dict_inventory[name].size() + 1):
+					# Item exists in inventory
+					if(DataResource.dict_inventory[name]["Item" + str(i)].item_name == loot_dict[index][1]):
+						print("Item Present")
+						DataResource.dict_inventory[name]["Item" + str(i)].item_qty += loot_dict[index][2]
+						break
+					# Item not present
+					elif(i == DataResource.dict_inventory[name].size()):
+						print("Item Not Present. Inserting...")
+						var curr_size = DataResource.dict_inventory[name].size() + 1
+						insert_data(index, curr_size)
 		index += 1
 		loot_count -= 1
 	DataResource.save_rest()
