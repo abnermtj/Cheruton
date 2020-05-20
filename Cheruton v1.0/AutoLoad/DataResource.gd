cbcb7454915c73e3e6d8c17f64472aafad3dbe4f
theopin <theopinto98@gmail.com>
2020-05-20 11:10:51 +0800

    playerdata is created if none present, refined reset

--- Cheruton v1.0/AutoLoad/DataResource.gd
+++ Cheruton v1.0/AutoLoad/DataResource.gd
@@ -5,40 +5,43 @@ extends Node
 #   DataResource.dict_x.feature = value
 
 const MAIN = "res://SaveData/player-data.json"
+const MASTERLIST = "res://SaveData/item-masterlist.json"
 
 var current_scene = null
-
+# Main Dict
 var dict_main = {}
 var dict_inventory = {}
-var dict_item_spawn = {}
 var dict_player = {}
 var dict_settings = {}
+# Masterlist Dict
+var dict_masterlist = {}
 var dict_item_masterlist = {}
-
+var dict_item_spawn = {}
 # Stores any unsaved data regarding player stats
 var temp_dict_player = {}
 
 func load_data():
 	#Editable
 	dict_main = load_dict(MAIN)
+	dict_masterlist = load_dict(MASTERLIST)
 	dict_player = dict_main.player.main
 	dict_settings = dict_main.settings.main
 	dict_inventory = dict_main.inventory
 	temp_dict_player = dict_player
 
 	#Non-Editable
-	dict_item_spawn = dict_main.item_spawn
-	dict_item_masterlist = dict_main.item_masterlist
+	dict_item_spawn = dict_masterlist.item_spawn
+	dict_item_masterlist = dict_masterlist.item_masterlist
 
 
 
 func load_dict(FilePath):
 	var DataFile = File.new()
+	if(FilePath == MAIN && !DataFile.file_exists(FilePath)): # create new save
+		save_data(FilePath, dict_main)
+		reset_all()
 	DataFile.open(FilePath, File.READ)
-	#if(!DataFile.file_exists(FilePath)):
-		#save_data(FilePath, dict_main)
-		#reset_all()
-	#var err = DataFile.open_encrypted_with_pass(FilePath, File.READ, "mypass")
+	#DataFile.open_encrypted_with_pass(FilePath, File.READ, "mypass")
 	var data = JSON.parse(DataFile.get_as_text())
 	DataFile.close()
 	print("Data Loaded!")
@@ -65,7 +68,13 @@ func restore_last_save():
 	temp_dict_player = dict_player
 
 func reset_all():
+	dict_main = {"player": {"main": {}}, "settings": {"main": {}}, "inventory": {} }
+	dict_main.player.main = dict_player
+	dict_main.settings.main = dict_settings
+	dict_main.inventory = dict_inventory
 	reset_player()
+	reset_settings()
+	reset_inventory()
 
 func reset_player():
 	dict_player.exp_curr = 0
@@ -74,6 +83,8 @@ func reset_player():
 	dict_player.health_max = 50
 	dict_player.level = 1
 	dict_player.coins = 10
+	dict_player.Weapons_item = null
+	dict_player.Apparel_item = null
 	save_player()
 
 func reset_settings():
@@ -81,4 +92,12 @@ func reset_settings():
 	dict_settings.is_mute = false
 	dict_settings.game_on = false
 	dict_settings.maj_scn = true
+	save_rest()
+
+func reset_inventory():
+	dict_inventory.Weapons =  {}
+	dict_inventory.Apparel = {}
+	dict_inventory.Consum = {} 
+	dict_inventory.Misc = {}
+	dict_inventory["Key Items"] = {}
 	save_rest()
