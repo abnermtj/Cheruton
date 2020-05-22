c12bf2d4a1241a1c36982232b4241a5aed55063d
theopin <theopinto98@gmail.com>
2020-05-22 18:54:25 +0800

    ok found how to switch mouse ptr

--- Cheruton v1.0/AutoLoad/Cursor.gd
+++ Cheruton v1.0/AutoLoad/Cursor.gd
@@ -4,10 +4,13 @@ extends Node
 var arrow = preload("res://Display/MouseDesign/arrow.png")
 var beam = preload("res://Display/MouseDesign/beam.png")
 
+# Changing cursor: 	set_default_cursor_shape(int btw 0 & 15)
 
 func init_cursor():
 	
 	# Changes the shapes of the cursor for various functions.
 	Input.set_custom_mouse_cursor(arrow)
 	Input.set_custom_mouse_cursor(beam, Input.CURSOR_IBEAM)
+	
+
 
