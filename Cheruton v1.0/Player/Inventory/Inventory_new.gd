c52bf60e531c853ca8aab9eb64e6fa9f8cfe3afd
theopin <theopinto98@gmail.com>
2020-05-26 21:25:35 +0800

    generating items for the screen (single line tested only)

--- Cheruton v1.0/Player/Inventory/Inventory_new.gd
+++ Cheruton v1.0/Player/Inventory/Inventory_new.gd
@@ -7,13 +7,23 @@ signal tab_changed(next_tab)
 onready var active_tab_image = preload("res://Player/Inventory/Icons/Button_Bg/inventory_bg_keypress.png")
 onready var default_tab_image = preload("res://Player/Inventory/Icons/Button_Bg/inventory_bg.png")
 
+onready var weapons_list = DataResource.dict_inventory.get("Weapons")
+onready var apparel_list = DataResource.dict_inventory.get("Apparel")
+onready var consum_list = DataResource.dict_inventory.get("Consum")
+onready var misc_list = DataResource.dict_inventory.get("Misc")
+onready var key_items_list = DataResource.dict_inventory.get("Key Items")
+
+
 onready var tabs = $Border/Bg/Contents/Tabs
+onready var items = $Border/Bg/Contents/Items
 onready var equipped_coins = $Border/Bg/Contents/EquippedCoins
 
 func _ready():
 	DataResource.dict_settings.game_on = false
 	connect_tabs()
+	load_data()
 	emit_signal("tab_changed", "Weapons")
+	
 	equipped_coins.get_node("CoinsVal").text = str(DataResource.temp_dict_player["coins"])
 	
 
@@ -54,7 +64,50 @@ func change_active_tab(new_tab):
 	# Set current tab to default colour and hide its items
 	if(active_tab):
 		active_tab.set_normal_texture(default_tab_image)
+		items.get_node(active_tab.name).hide()
+	
 	# Set new active tab and its colour and show its items
 	active_tab = new_tab
-
 	active_tab.set_normal_texture(active_tab_image)
+	items.get_node(active_tab.name).show()
+	print("OK")
+
+func load_data():
+	#Find subnodes of each tab
+	var weapons_scroll = items.get_node("Weapons/Column")
+	var apparel_scroll = items.get_node("Apparel/Column")
+	var consum_scroll = items.get_node("Consum/Column")
+	var misc_scroll = items.get_node("Misc/Column")
+	var key_items_scroll = items.get_node("KeyItems/Column")
+
+	#Generate list of items based on tab
+	generate_list(weapons_scroll, weapons_list, 100)
+	generate_list(apparel_scroll, apparel_list, 200)
+	generate_list(consum_scroll, consum_list, 300)
+	generate_list(misc_scroll, misc_list, 400)
+	generate_list(key_items_scroll, key_items_list, 500)
+
+func generate_list(scroll_tab, list_tab, tab_index):
+	var index = 1
+	var row_index = 0
+	for _i in range(0, list_tab.size()):
+		# New Row
+		if(index / 10 != row_index && index % 10 != 0): 
+			row_index += 1
+			var new_row = HBoxContainer.new()
+			scroll_tab.add_child(new_row)
+			scroll_tab.get_child(get_node(scroll_tab).get_child_count() - 1).name = "Row" + str(row_index)
+		
+		var row = scroll_tab.get_node("Row" + str(row_index))
+		
+		# New Item
+		var instance_loc = load("res://Player/Inventory/101.tscn")
+		var instanced = instance_loc.instance()
+		row.add_child(instanced)
+		row.get_child(row.get_child_count() - 1).name = str(tab_index + index)
+
+		#add data
+
+		index += 1
+	# Hide data
+	scroll_tab.get_parent().hide()
