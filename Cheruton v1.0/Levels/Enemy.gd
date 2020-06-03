569e13c7b9f0f6796f8474e9a3ddf86ef155a016
theopin <theopinto98@gmail.com>
2020-06-03 17:36:53 +0800

    sum enemy basic properties

---
+++ Cheruton v1.0/Levels/Enemy.gd
@@ -0,0 +1,74 @@
+extends KinematicBody2D
+
+onready var hp_bar
+onready var player
+
+var max_HP = 400#stub
+var curr_HP
+var percent_HP
+var can_heal = true
+var is_dead = false
+var state = "Ignore"
+
+var player_nearby
+var player_spotted
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#$AnimationPlayer.play("state_idle")
+	curr_HP = max_HP
+
+
+
+func _process(delta):
+	if(percent_HP <= 50 && can_heal):
+		heal_enemy()
+	else:
+		match state:
+			"Ignore":
+				print("Ignored")
+			"Search":
+				pass
+			"Follow":
+				pass
+			"Attack":
+				print("BamBAM")
+
+
+func heal_enemy():
+	can_heal = false
+	yield(get_tree().create_timer(0.25), "timeout")
+	if(!is_dead):
+		var skill
+		var skill_instance = skill.instance()
+		skill_instance.skill_name = "Heal"
+		add_child(skill_instance)
+		yield(get_tree().create_timer(0.25), "timeout")
+		can_heal = true
+
+func _physics_process(delta):
+	LOSCheck()
+	
+func _on_Sight_body_entered(body):
+	if(body == player):
+		player_nearby = true
+	
+func _on_Sight_body_exited(body):
+	if(body == player):
+		player_nearby = false
+
+#Checks if the player is close enough to be attaced
+func LOSCheck():
+	if player_nearby:
+		var space_state = get_world_2d().direct_space_state
+		var LOSight_id = space_state.intersect_ray(position, player.position)
+		if LOSight_id:
+			if (LOSight_id.collider.name == "Player"):
+				player_spotted = true
+				state = "Attack"
+			else:
+				player_spotted = false
+				state = "Ignore" #Stub
+
+
