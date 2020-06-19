a1316a37f7908284c20d7577b42f78c63cac0a8a
theopin <theopinto98@gmail.com>
2020-06-19 08:31:45 +0800

    bat attack foundation

---
+++ Cheruton v1.0/Enemy/Mobs/Bat/Attack/BatRange.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+const SPEED = 400
+
+var velocity = Vector2()
+onready var player = get_parent().get_parent().get_node("player")
+
+func _ready():
+	velocity.y = 0
+	velocity.x = player.global_position * SPEED
+
+func _process(delta):
+	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN * 8, Vector2.UP)
+
+func _on_HitBox_body_entered(body):
+	self.queue_free()
+
