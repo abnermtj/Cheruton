76d6da21de480acb8450a539430977917008c915
theopin <theopinto98@gmail.com>
2020-06-27 15:41:55 +0800

    floaing text works

--- Cheruton v1.0/Props/DamageVal/DamageVal.gd
+++ Cheruton v1.0/Props/DamageVal/DamageVal.gd
@@ -8,11 +8,12 @@ var text_velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	value.set_text(str(abs(amount)))
 	
 	randomize()
-	#var angle = randi()%21 - 20 # Dirn btw -20 to 20 degress
-	text_velocity = Vector2(0, -6)
+	var angle = randi()%81 - 40 # Dirn btw -40 to 40 degress
+	text_velocity = Vector2(angle, -100)
 	tween.interpolate_property(self, "scale", scale, Vector2(0.30, 0.30), 0.2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.interpolate_property(self, "scale", Vector2(0.3, 0.3), Vector2(0.1, 0.1), 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0.3)
 	tween.start()
