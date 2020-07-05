6d85a57e24ea82309a11eb9bb6958113b0d60292
theopin <theopinto98@gmail.com>
2020-07-05 19:47:08 +0800

    implemented slider tween but buggy

--- Cheruton v1.0/Display/MainMenu/MainMenu.gd
+++ Cheruton v1.0/Display/MainMenu/MainMenu.gd
@@ -9,14 +9,25 @@ const HEALTHBAR = "HudLayer/Hud/StatBars/HealthBar"
 onready var settings = $Settings
 onready var options = $Background/Options
 onready var timer_options = $Timer
+onready var slider = $Background/Options/Slider
+onready var tween = $Background/Options/Slider/Tween
+
 onready var white_screen = $WhiteScreen
 onready var options_delay = $OptionsDelay
+
+onready var play_position = $Background/Options/VBoxContainer/Play.rect_position
+onready var settings_position = $Background/Options/VBoxContainer/Settings.rect_position
+onready var quit_position = $Background/Options/VBoxContainer/Quit.rect_position
+
 
 onready var bg_music_fill
 
 var modulate_dec = "white"
+var slider_active := false
+var tween_status := false
 
 func _process(delta):
+	print(modulate_dec)
 	#white_screen.modulate.a -= 0.01
 	modulate_func()
 
@@ -47,4 +58,33 @@ func _on_Timer_timeout():
 	options.show()
 
 
+func _on_Play_mouse_entered():
+	var new_position = Vector2(slider.rect_position.x, play_position.y)
+	slide_to_position(new_position)
 
+func _on_Settings_mouse_entered():
+	var new_position = Vector2(slider.rect_position.x, settings_position.y)
+	slide_to_position(new_position)
+
+func _on_Quit_mouse_entered():
+	var new_position = Vector2(slider.rect_position.x, quit_position.y)
+	slide_to_position(new_position)
+
+# Slides the slider to the intended position, or shows it there if not visible
+func slide_to_position(new_position):
+	if(!tween_status):
+		var old_position = slider.rect_position
+		if(slider_active):
+			print("Lolol")
+			tween.interpolate_property(slider, "rect_position", old_position, new_position, 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			tween_status = true
+			tween.start()
+		else:
+			print("loLLLLL")
+			old_position.y = new_position.y
+			slider.show()
+			slider_active = true
+
+
+func _on_Tween_tween_completed(object, key):
+	tween_status = false
