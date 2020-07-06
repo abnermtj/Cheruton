2ac9a06b5cbeb9d75c3ffe7083b8f3dd171aa43d
theopin <theopinto98@gmail.com>
2020-07-06 11:38:24 +0800

    fixes for mmenu

--- Cheruton v1.0/Display/MainMenu/MainMenu.gd
+++ Cheruton v1.0/Display/MainMenu/MainMenu.gd
@@ -15,6 +15,8 @@ onready var tween = $Background/Options/Slider/Tween
 onready var white_screen = $WhiteScreen
 onready var options_delay = $OptionsDelay
 
+onready var container = $Background/Options/VBoxContainer
+
 onready var play_position = $Background/Options/VBoxContainer/Play.rect_position
 onready var settings_position = $Background/Options/VBoxContainer/Settings.rect_position
 onready var quit_position = $Background/Options/VBoxContainer/Quit.rect_position
@@ -47,6 +49,7 @@ func modulate_func():
 		"options":
 			if(options.modulate.a > 0.99):
 				modulate_dec = "idle"
+				enable_options()
 			else:
 				options.modulate.a += 0.03
 
@@ -54,6 +57,11 @@ func modulate_func():
 func _on_OptionsDelay_timeout():
 	white_screen.hide()
 	modulate_dec = "options"
+	
+#Enables the Buttons for use
+func enable_options():
+	for i in container.get_child_count():
+		container.get_child(i).disabled = false
 
 func _on_Play_pressed():
 	SceneControl.emit_signal("init_statbar")
@@ -87,17 +95,16 @@ func _on_Quit_mouse_entered():
 
 # Slides the slider to the intended position, or shows it there if not visible
 func slide_to_position(new_position):
-	print(new_position)
+	# Offset of position
+	new_position.y += container.rect_position.y
 	if(!tween_status):
 		var old_position = slider.rect_position
 		if(slider_active):
-			print("Lolol")
 			tween.interpolate_property(slider, "rect_position", old_position, new_position, 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 			tween_status = true
 			tween.start()
 		else:
-			print("loLLLLL")
-			old_position.y = new_position.y
+			slider.rect_position.y = new_position.y
 			slider.show()
 			slider_active = true
 
