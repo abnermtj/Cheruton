a887092202c61daddadaa4c4454c45562ea51794
theopin <theopinto98@gmail.com>
2020-07-28 16:26:55 +0800

    shop new method

--- Cheruton v1.0/Levels/Hometown/NPCs/clgirl/clgirl.gd
+++ Cheruton v1.0/Levels/Hometown/NPCs/clgirl/clgirl.gd
@@ -1,4 +1,10 @@
 extends StaticNPC
 
+func _ready():
+	var _connect_0 = DataResource.connect("dialog_over", self, "_on_dialog_processed")
+
 func interact(body):
 	SceneControl.change_and_start_dialog(name) # else his anvil will also rotate
+
+func _on_dialog_processed():
+	emit_signal("new_gui", "shop")
